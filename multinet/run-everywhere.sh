149af7ffe578e24ad4158ca03247c28f519d12db
monchkrit <monchkrit@gmail.com>
2020-08-03 04:34:17 +0000

    remote execution script

---
+++ multinet/run-everywhere.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+# Server List
+SERVER_LIST='servers'
+
+# Options for the ssh command.
+SSH_OPTIONS='-o ConnectTimeout=2'
+
+usage() {
+	# Display the usage and exit.
+	echo "Usage: ${0} [-nsv] [-f FILE] COMMAND" >&2
+	echo "Executes COMMAND as a single command on every server.' >&2"
+	echo " -f FILE Use FILE for the list of servers. Default &{SERVER_LIST}." >&2
+	echo ' -n	Dry run mode. Display the COMMAND that would have been executed and exit.' >&2
+	echo ' -s	Execute the COMMAND using sudo on the remove server.' >&2
+	echo ' -v	Verbose mode. Displays the server name before executing COMMAND.' >&2
+	exit 1
+}
+
+# Make sure the script is not executed with super user privileges.
+if [[ "${UID}" -eq 0 ]]
+then
+	echo 'Do not execute this script as root. Use the -s option instead.' >&2
+	usage
+fi
+
+# Parse the options.
+while getopts f:nsv OPTION
+do
+	case ${OPTION} in
+		f) SERVER_LIST="${OPTARG}" ;;
+		n) DRY_RUN='true' ;;
+		s) SUDO='sudo' ;;
+		v) VERBOSE='true' ;;
+		?) usage ;;
+	esac
+done
+
+# Remove the options while leaving the remaining arguments.
+shift "$(( OPTIND - 1 ))"
+
+# If the user doesn't supply at least one argument, give them help.
+if [[ "${#}" -lt 1 ]]
+then
+	usage
+fi
+
+# Anything that remains on the command line is the command to execute on the servers.
+COMMAND="${@}"
+
+# Make sure the SERVER_LIST file exists.
+if [[ ! -e "${SERVER_LIST}" ]]
+then
+	echo "Cannot open server list file ${SERVER_LIST}." >&2
+	exit 1
+fi
+
+# Expect the success status
+EXIT_STATUS='0'
+
+# Loop through the SERVER_LIST
+for SERVER in $(cat ${SERVER_LIST})
+do
+	if [[ "${VERBOSE}" = 'true' ]]
+	then
+		echo "${SERVER}"
+	fi
+
+	SSH_COMMAND="ssh ${SSH_OPTIONS} vagrant@${SERVER} ${SUDO} ${COMMAND}"
+
+	# If it's a dry run, then show the command, but don't execute it.
+	if [[ "${DRY_RUN}" = 'true' ]]
+	then
+		echo "DRY RUN: ssh ${SSH_COMMAND}"
+	else
+		${SSH_COMMAND}
+		SSH_EXIT_STATUS="${?}"
+
+		# Capture any exit status that is non-zero and report to the user.
+		if [[ "${SSH_EXIT_STATUS}" -ne 0 ]]
+		then
+			EXIT_STATUS="${SSH_EXIT_STATUS}"
+			echo "Execution on ${SERVER} failed." >&2
+		fi
+	fi
+done
+
+exit ${EXIT_STATUS}
+
